73e2e2b7a9d9fb387670680d6d714faed793ff65
vlhobbs <vlhobbs@uark.edu>
2019-04-12 15:49:15 -0500

    Implemented INotifyPropertyChanged interface

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class Player
+    public class Player :INotifyPropertyChanged
+
     {
-        string Name { get; set; }
-        string CharacterClass { get; set; }
-        int HitPoints { get; set; }
-        int ExperiencePoints { get; set; }
-        int Level { get; set; }
-        int Tokens { get; set; }
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _experiencePoints;
+        private int _level;
+        private int _tokens;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+
+        public int ExperiencePoints
+        {
+            get { return _experiencePoints; }
+            set
+            {
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+
+        public int Tokens
+        {
+            get { return _tokens; }
+            set
+            {
+                _tokens = value;
+                OnPropertyChanged("Tokens");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        //later properties: Strength, Dexterity
+        //Strength adds to attack power and is higher on androids
+        //Dexterity adds to defense and is higher on humans
     }
 }
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.CharacterClass = "Cyborg";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Tokens = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
